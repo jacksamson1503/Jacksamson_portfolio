1993b3d0765e22540d5596366f6456237bd2b2ef
jacksamson1503 <jacksamsonc@gmail.com>
2026-01-31 13:14:53 +0530

    Rename variable 'reveals' to 'sections'

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const reveals = document.querySelectorAll(".reveal");
+const sections = document.querySelectorAll(".reveal");
 
 const revealOnScroll = () => {
   const windowHeight = window.innerHeight;
-  reveals.forEach(section => {
+  sections.forEach(section => {
     const top = section.getBoundingClientRect().top;
     if (top < windowHeight - 100) {
       section.classList.add("active");
